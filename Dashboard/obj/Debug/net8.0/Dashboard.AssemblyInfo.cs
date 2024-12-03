1a2779891347e028f11ccb0165ca7e34c35ba047
Saravanan0511 <121454742+Saravanan0511@users.noreply.github.com>
2024-12-03 17:17:18 +0530

    On Dec 3rd , Changed Some Qry's and Ui

--- Dashboard/obj/Debug/net8.0/Dashboard.AssemblyInfo.cs
+++ Dashboard/obj/Debug/net8.0/Dashboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf94702ad84051df80097a323b56b670dcc22665")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d78ed4cbcc8fbb2237164b8292266356c0188b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
