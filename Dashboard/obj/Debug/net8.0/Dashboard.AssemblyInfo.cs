a8f700212395682f987d3010d4bca09d9b14f27e
Saravanan0511 <121454742+Saravanan0511@users.noreply.github.com>
2025-01-13 16:36:46 +0530

    Full TransformerWise Completed

--- Dashboard/obj/Debug/net8.0/Dashboard.AssemblyInfo.cs
+++ Dashboard/obj/Debug/net8.0/Dashboard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dashboard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d70bc668486ebad086d5828dec9c9e5924634222")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7883bb18a8201a78cc89fe53ab6cb32eb9d07a2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dashboard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dashboard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
